3823e5b91fb0aedcb392fa0c35652b39ba10e102
Daniel <p_daniel@hotmail.it>
2019-11-06 12:01:25 +0100

    implement fight between players

--- back/pokedex/api/matches.py
+++ back/pokedex/api/matches.py
@@ -4,12 +4,11 @@ from flask_restful import Resource
 from pokedex.managers.collections import get_user_by_name
 from pokedex.managers.matches import fight
 
-# from pokedex.managers.collections import get_pokemonscollection_by_name, delete_pokemon_from_collection, create_new_user, \
-#     get_user_by_name, create_a_new_collection, get_collection_by_name, add_pokemon_to_collection, get_pokemons_from_collection
+
 
 class Play(Resource):
     def get(self):
-        match_log=[]
+        match_log = []
 
         player1_name = request.args['player1']
         player2_name = request.args['player2']
@@ -25,8 +24,6 @@ class Play(Resource):
         print(player2.name)
         match_log.append("Player2 is " + player2.name)
 
-        text_result=fight(player1,player2)
+        text_result = fight(player1, player2)
         match_log.append(text_result)
         return match_log
-
-
