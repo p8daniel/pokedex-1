7679ffb250818c93d90f35d37328338d17e4c9fa
Daniel <p_daniel@hotmail.it>
2019-11-15 10:10:27 +0100

    improve pokemon scrapping

--- back/pokedex/managers/scrapping.py
+++ back/pokedex/managers/scrapping.py
@@ -17,18 +17,27 @@ def load_pokemons_from_wikipedia():
     pokemons_table_rows = pokemons_table.findall('.//tr')
 
     pokemons = {}
-    generations = ['generation-i', 'generation-ii', 'generation-iii', 'generation-iv', 'generation-v', 'generation-vi',
-                   'generation-vii', 'generation-viii']
+    generations = []
+    for elem in pokemons_table_rows[0]:
+        generations.append(elem.text_content())
+    # generations = ['generation-i', 'generation-ii', 'generation-iii', 'generation-iv', 'generation-v', 'generation-vi',
+    #                'generation-vii', 'generation-viii']
 
     specie_symbols = {'': None, '※': 'Baby Pokémon', '♭': 'Mythical Pokémon', '♯': 'Ultra Beast', '~': 'Fossil Pokémon'}
     specie_colors = {'background-color: #9FCADF': 'Starter Pokémon', 'background-color: #E89483': 'Legendary Pokémon'}
+
     for row in pokemons_table_rows[2:]:
 
         pokemon_id = None
 
         i = 0
-        gen_cont = 0
+
         for column in row.findall('td'):
+
+            image = column.findall('img')
+            if len(image) > 0:
+                img_identifier = image[0].get('alt')
+                print(img_identifier)
 
             if i % 2 == 0:
                 content = column.text_content()
@@ -37,7 +46,7 @@ def load_pokemons_from_wikipedia():
                     pokemons[pokemon_id] = None
                 else:
                     i += 1
-                    gen_cont += 1
+
             else:
                 symbols_to_strip = ['※', '♭', '♯', '~']
                 pokemon_name = column.text_content()
@@ -46,12 +55,9 @@ def load_pokemons_from_wikipedia():
                 pokemon_name = pokemon_name.strip('[e]')
                 for symbol in symbols_to_strip:
                     if symbol in pokemon_name:
-                        print(pokemon_name)
-                        print(symbol)
                         pokemon_name = pokemon_name.strip(symbol)
-                        print(pokemon_name)
                         pokemon_symbol = symbol
-                pokemon_gen = generations[gen_cont]
+                pokemon_gen = generations[int((i - 1) / 2)]
 
                 pokemon_name = pokemon_name.rstrip()
                 if pokemon_id is not None:
@@ -66,7 +72,6 @@ def load_pokemons_from_wikipedia():
                         pokemons[pokemon_id].append(specie_symbols[pokemon_symbol])
 
                     pokemon_id = None
-                    gen_cont += 1
 
             i += 1
 
